66d6f442e3244ca47dd475e63e61e95b44bf23c5
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2025-12-26 22:48:23 -0500

    More Documentation

--- src/Dropdown.tsx
+++ src/Dropdown.tsx
@@ -1,17 +1,35 @@
 import React from "react";
 import {MenuItem, Select} from "@mui/material";
 
+/**
+ * Element representing an item in a dropdown
+ * @param label - The label to display for the dropdown item
+ * @param value - The value associated with the dropdown item
+ * @param key - Optional key for the dropdown item, defaults to value if not provided
+ */
 export interface DropdownItem<T extends string | number> {
     label: string;
     value: T;
     key?: string;
 }
 
+/**
+ * Props for the Dropdown component
+ * @param items - The items to display in the dropdown
+ * @param onChange - Callback function to invoke when an item is selected
+ */
 export interface DropdownProps<T extends string | number> {
     items: DropdownItem<T>[];
     onChange: (value: T) => void;
 }
 
+/**
+ * Dropdown component
+ * @param items - The items to display in the dropdown
+ * @param onChange - Callback function to invoke when an item is selected
+ * @param selectProps - Additional props to pass to the underlying Select component
+ * @constructor
+ */
 export function Dropdown<T extends string | number>({items, onChange, ...selectProps}: DropdownProps<T>): React.JSX.Element {
     return (
         <Select {...selectProps}>
